c38f36c91a0c0962289139db51662301c6481447
phzwart <PHZwart@lbl.gov>
2023-05-09 11:44:22 -0700

    Cookiecutter template

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='latexp',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'dash',
+        'pandas',
+        'plotly'
+    ],
+    entry_points={
+        'console_scripts': [
+            'latexp=app.index:main'
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False
+)
